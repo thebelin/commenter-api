716f6e9f1bc8c11e28861bd9df557464e1840e7c
Belin Fieldson <thebelin@gmail.com>
2014-12-07 18:23:29 -0700

    This view is useful

---
+++ app/views/comments.php
@@ -0,0 +1,48 @@
+<div
+    data-ng-app        = "CommenterApp"
+    data-ng-controller = "CommenterController"
+
+    class              = "commentBox">
+    <div class="welcome">
+        <b>Comments:</b>
+    </div>
+    <!-- This is the infinite scrolling content -->
+    <div class = "commentList" id = "commentList"
+        data-ng-scroll = "listScroll()">
+        <div
+            data-ng-repeat      = "comment in comments"
+            data-boundary-links = "true"
+            class               = "commenterItem"   
+            data-message-id     = "{{comment.id}}" >
+                <img 
+                  class      = "gravatarImage"
+                  data-ng-src = "http://www.gravatar.com/avatar/{{comment.gravatar}}.png"
+                  data-email = "{{comment.email}}" >
+                  <!-- THis should contain the gravatar image -->
+                </img>
+                <div class = "commenterComment">
+                    {{comment.message}}
+                </div>
+        </div>
+    </div>
+    <!-- This is the end of the infinite scrolling content -->
+    <div class = "commenterForm">
+        <label for = "commenterComment">Your Comment:</label>
+        <textarea id = "commenterCommentEntry" data-ng-model = "commentItem.message"></textarea>
+        <div data-ng-if = "commentItem.message">
+            <label for = "commenterEmail">Your Email:</label>
+            <input type = "text" name = "email" data-ng-model = "commentItem.email"/>
+        </div>
+        <div
+            data-ng-if = "commentItem.email"
+            vc-recaptcha
+            theme    = "clean"
+            lang     = "en"
+            ng-model = "commentItem.captcha"
+            key      = "'6Lf10f0SAAAAAF1wRn6VEjGvr6YTnH_XypcTmPrs'"
+        ></div>
+        <input class = "commenterButton" type = "button"
+            data-ng-if = "commentItem.captcha"
+            data-ng-click = "addComment()" value = "Comment"></input>
+    </div>
+</div>
